6443dce48779762fa79733953844d216762dbcbf
jhonatanMax <max093006@gmail.com>
2026-09-22 07:58:32 -0500

    entrega parcial

--- src/ejercicio1.java
+++ src/ejercicio1.java
@@ -3,8 +3,13 @@
 
 public class ejercicio1 {
     public static void main(String[] args) throws Exception {
-       
-        int posicion = 10;
+        System.out.println("escriba su numero de paquetes por hora: ");
+        int [] pocisiones = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,};
+
+    for (int i = 10; i <10; i++) {
+    }
+
+    }
         
     }
-}
+
